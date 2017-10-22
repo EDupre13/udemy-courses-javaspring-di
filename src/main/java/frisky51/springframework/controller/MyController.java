30153eba826aa60ef4545639be5e30a4af4e3fb7
EDupre13 <EDupre13@gmail.com>
2017-10-22 09:22:53 -0500

    Hello World example

---
+++ src/main/java/frisky51/springframework/controller/MyController.java
@@ -0,0 +1,10 @@
+package frisky51.springframework.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public void hello() {
+        System.out.println("Hello, World!");
+    }
+}
